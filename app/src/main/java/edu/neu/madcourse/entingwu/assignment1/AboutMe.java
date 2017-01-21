1c307033530c90fbea2cd0e72b5bf26c852689c4
EntingWu <wu.en@husky.neu.edu>
2017-01-20 17:41:11 -0800

    Get the IMET of the device that the app is running on.

--- app/src/main/java/edu/neu/madcourse/entingwu/assignment1/AboutMe.java
+++ app/src/main/java/edu/neu/madcourse/entingwu/assignment1/AboutMe.java
@@ -1,7 +1,11 @@
 package edu.neu.madcourse.entingwu.assignment1;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+
 import edu.neu.madcourse.entingwu.R;
 
 public class AboutMe extends AppCompatActivity {
@@ -10,5 +14,11 @@ public class AboutMe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_me);
+
+        TextView textView = (TextView) findViewById(R.id.textView_phoneid);
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String author_phoneid = telephonyManager.getDeviceId();
+        textView.setText(author_phoneid);
+        System.out.println(author_phoneid);
     }
 }
