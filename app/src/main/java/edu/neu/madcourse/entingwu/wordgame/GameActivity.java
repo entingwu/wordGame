c6fca5244c128699275085a91ffb01fa75d125e3
EntingWu <wu.en@husky.neu.edu>
2017-03-13 16:31:14 -0700

    Query the highest score in database and send notification

--- app/src/main/java/edu/neu/madcourse/entingwu/wordgame/GameActivity.java
+++ app/src/main/java/edu/neu/madcourse/entingwu/wordgame/GameActivity.java
@@ -9,21 +9,41 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 import edu.neu.madcourse.entingwu.R;
 import edu.neu.madcourse.entingwu.firebase.models.Game;
 
 public class GameActivity extends Activity {
 
+    // Please add the server key from your firebase console in the follwoing format "key=<serverKey>"
+    private static final String SERVER_KEY = "key=AAAAyUeo0PE:APA91bEf8-5uFFKVxuP7RsqI-zZhUZAoAIyY9eU5myfNq4nLrBTU7e8ECauhk8Iu6IFLCC3nWmiSqn6snWOnsTzS7LicevQpa2QAWw5IMeL5IIODcXNS0W7m4pziaWj3EdG7H5t0U73-";
     private static final String TAG = GameActivity.class.getSimpleName();
+    private static final String SCORE = "score";
+    private static final String GAMES = "games";
     private DatabaseReference mDatabase;
     private MediaPlayer mMediaPlayer;
     private Handler mHandler = new Handler();
@@ -32,12 +52,12 @@ public class GameActivity extends Activity {
     private ArrayAdapter<String> adapter;
     private String userName;
     private Game game;
-    List<String> listItems = new ArrayList<>();
-    Dictionary dictionary = new Dictionary();
-    TextView tx;
-    TextView timerText;
-    CountDownTimer timer;
-    long totalSec;
+    public List<String> listItems = new ArrayList<>();
+    public Dictionary dictionary = new Dictionary();
+    public TextView tx;
+    public TextView timerText;
+    public CountDownTimer timer;
+    public long totalSec;
 
 @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -147,11 +167,90 @@ public class GameActivity extends Activity {
         game = new Game(userName, String.valueOf(score), String.valueOf(mGameFragment.scorePhase1),
                 String.valueOf(mGameFragment.scorePhase2), mGameFragment.longestWord,
                 String.valueOf(mGameFragment.wordScore));
-        mDatabase.child("games").child(game.id).setValue(game);
+        mDatabase.child(GAMES).child(game.id).setValue(game);
         Log.i(TAG, "onAddScore: " + game.score);
 
-        /** 3. Send high score notification */
-
+        /** 3. Query the highest score and send notification */
+        queryHighScore();
     }
 
+    private void queryHighScore() {
+        Query query = mDatabase.child(GAMES).orderByChild(SCORE).limitToLast(1);
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot childDataSnapshot : dataSnapshot.getChildren()) {
+                        Game game = childDataSnapshot.getValue(Game.class);
+                        pushNotification(game.userName, game.score);
+                        Log.i(TAG, "Old high score: " + game.score +
+                                " ,New high score: " + mGameFragment.score);
+                        //if (mGameFragment.score >= Integer.valueOf(game.score)) {}
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
+    }
+
+    public void pushNotification(final String userName, final String score) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                pushNotificationTask(userName, score);
+            }
+        }).start();
+    }
+
+    public void pushNotificationTask(final String userName, String score) {
+        JSONObject jPayload = new JSONObject();
+        JSONObject jNotification = new JSONObject();
+        try {
+            jNotification.put("title", "Google I/O 2016");
+            jNotification.put("body", userName + " gets " + score + ". Say Congratulations!");
+            jNotification.put("sound", "default");
+            jNotification.put("badge", "1");
+            jNotification.put("click_action", "OPEN_ACTIVITY_1");
+
+            // If sending to a single client
+            //jPayload.put("to", CLIENT_REGISTRATION_TOKEN);
+            jPayload.put("to", "/topics/news");
+            jPayload.put("priority", "high");
+            jPayload.put("notification", jNotification);
+
+            URL url = new URL("https://fcm.googleapis.com/fcm/send");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Authorization", SERVER_KEY);
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoOutput(true);
+
+            // Send FCM message content.
+            OutputStream outputStream = conn.getOutputStream();
+            outputStream.write(jPayload.toString().getBytes());
+            outputStream.close();
+
+            // Read FCM response.
+            InputStream inputStream = conn.getInputStream();
+            final String resp = convertStreamToString(inputStream);
+
+            Handler h = new Handler(Looper.getMainLooper());
+            h.post(new Runnable() {
+                @Override
+                public void run() {
+                    Log.e(TAG, "run: " + resp);
+                    Toast.makeText(GameActivity.this, "New high score!", Toast.LENGTH_LONG).show();
+                }
+            });
+
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String convertStreamToString(InputStream is) {
+        Scanner s = new Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next().replace(",", ",\n") : "";
+    }
 }
